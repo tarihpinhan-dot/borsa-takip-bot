8e6f7c19d67bd7800d3c9e48edd230bb31678d94
tarihpinhan-dot <tarihpinhan@gmail.com>
2026-07-06 22:46:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Bot başlıyor...")
+import os
+import requests
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+data = {
+    "chat_id": CHAT_ID,
+    "text": "✅ Tebrikler! Bot başarıyla çalışıyor.\n\nBu mesaj GitHub Actions tarafından gönderildi."
+}
+
+response = requests.post(url, data=data)
+
+print(response.text)
