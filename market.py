aa69894eb830978c6526e95c3582b8fe523a59ae
tarihpinhan-dot <tarihpinhan@gmail.com>
2026-07-07 00:30:02 +0300

    Update market.py

--- market.py
+++ market.py
@@ -1,14 +1,22 @@
 import pandas as pd
+import requests
+from io import StringIO
 
 
 def get_sp500_tickers():
     url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-    tables = pd.read_html(url)
 
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(url, headers=headers, timeout=30)
+    response.raise_for_status()
+
+    tables = pd.read_html(StringIO(response.text))
     df = tables[0]
-    tickers = df["Symbol"].tolist()
 
-    # Yahoo/Finnhub uyumu için noktalı sembolleri düzelt
+    tickers = df["Symbol"].tolist()
     tickers = [t.replace(".", "-") for t in tickers]
 
     return tickers
